8f7dfe2c81a87cda6956c37fa69cf761509ba736
jon-infante <jonjoninfante@gmail.com>
2022-01-16 15:53:39 -0500

    Get all stores api

---
+++ WebAPI/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using BL;
+using DL;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : Controller
+    {
+        private ISBL _sbl;
+        public StoreController(ISBL sbl)
+        {
+            _sbl = sbl;
+        }
+        // GET: api/<StoreController>
+        [HttpGet]
+        public List<Store> Get()
+        {
+            return _sbl.GetAllStores();
+        }
+
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,34 @@
+using DL;
+using BL;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Add references to interface repos
+builder.Services.AddScoped<ISRepo>(ctx => new DBStoreRepo(builder.Configuration.GetConnectionString("P1DB")));
+builder.Services.AddScoped<ISBL, StoreBL>();
+builder.Services.AddScoped<IURepo>(ctx => new DBUserRepo(builder.Configuration.GetConnectionString("P1DB")));
+builder.Services.AddScoped<IUBL, UserBL>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
